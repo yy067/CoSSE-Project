4e87b390c89bb59e9b64513bf8c5b3e1a2bd92b0
yy067 <1344903762@qq.com>
2026-09-22 16:25:21 +0800

    Update script to use main_classify.py

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #  run isic
-python main_classify63_ham.py --exp_name full_isic --use_vpt --use_pbert --fuse_method mope --train_instructor --dataset isic2019 --prompt_length 6 --moe_n_experts 4 --t_prompt_length 4 --lr_vis 4e-4 --lr_text 5e-4 --w_imp 0.01 --use_instruct --max_epoch 30
+python main_classify.py --exp_name full_isic --use_vpt --use_pbert --fuse_method mope --train_instructor --dataset isic2019 --prompt_length 6 --moe_n_experts 4 --t_prompt_length 4 --lr_vis 4e-4 --lr_text 5e-4 --w_imp 0.01 --use_instruct --max_epoch 30
 
 #  run ham
-python main_classify63_ham.py --exp_name full_ham --use_vpt --use_pbert --fuse_method mope --train_instructor --dataset ham10000 --prompt_length 6 --moe_n_experts 4 --t_prompt_length 4 --lr_vis 4e-4 --lr_text 5e-4 --w_imp 0.01 --use_instruct --max_epoch 30
+python main_classify.py --exp_name full_ham --use_vpt --use_pbert --fuse_method mope --train_instructor --dataset ham10000 --prompt_length 6 --moe_n_experts 4 --t_prompt_length 4 --lr_vis 4e-4 --lr_text 5e-4 --w_imp 0.01 --use_instruct --max_epoch 30
 
 #  run food101
-python main_classify63_ham.py --exp_name full_food101 --use_vpt --use_pbert --fuse_method mope --train_instructor --dataset food --prompt_length 6 --moe_n_experts 4 --t_prompt_length 4 --lr_vis 4e-4 --lr_text 5e-4 --w_imp 0.01 --use_instruct
+python main_classify.py --exp_name full_food101 --use_vpt --use_pbert --fuse_method mope --train_instructor --dataset food --prompt_length 6 --moe_n_experts 4 --t_prompt_length 4 --lr_vis 4e-4 --lr_text 5e-4 --w_imp 0.01 --use_instruct
 
 # Monitor the training process
 python -m tensorboard.main --logdir ./logs --port 6006
